0be15add0fbbb65130ee413bc48ab4ea59874743
gent0807 <theloopholesnk@gmail.com>
2024-12-19 20:32:34 +0900

    [Test]: 포인트 사용 내역 조회 테스트 코드
    Close #123

--- src/test/java/io/hhplus/tdd/point/PointService.java
+++ src/test/java/io/hhplus/tdd/point/PointService.java
@@ -5,14 +5,26 @@ import io.hhplus.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PointService {
 
     private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
+
+    long MAX_POINT = 5_000_000;
 
     public UserPoint selectById(long id) {
 
         return userPointTable.selectById(id);
     }
+
+    public List<PointHistory> selectAllById(long userId) {
+        return pointHistoryTable.selectAllByUserId(userId);
+    }
+
+
+
 }
--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -3,6 +3,7 @@ package io.hhplus.tdd.point;
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
@@ -10,21 +11,27 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 public class PointServiceTest {
 
     // 의존 상황의 대역 생성
     UserPointTable userPointTable = mock(UserPointTable.class);
 
+    // 의존 상황의 대역 생성
+    PointHistoryTable pointHistoryTable = mock(PointHistoryTable.class);
+
     // 테스트 상황 설정
-    PointService pointService = new PointService(userPointTable);
+    PointService pointService = new PointService(userPointTable, pointHistoryTable);
+
 
     @DisplayName("임의의 long type data를 id에 input => 해당 id에 맞는 UserPoint return")
     @Test
@@ -33,6 +40,10 @@ public class PointServiceTest {
         assertSelectUserPoint(new Random().nextLong(), 100L, System.currentTimeMillis());
 
         assertSelectUserPoint( new Random().nextLong(), 1_000_000L, System.currentTimeMillis());
+
+        assertSelectUserPoint(Long.MAX_VALUE, 100L, System.currentTimeMillis());
+
+        assertSelectUserPoint(Long.MIN_VALUE, -1L, System.currentTimeMillis());
 
     }
 
@@ -49,6 +60,34 @@ public class PointServiceTest {
                         .containsExactly(id, point, updateMillis);
 
         Mockito.verify(userPointTable).selectById(id);
+    }
+
+    @DisplayName("임의의 long type data를 user id에 input => 해당 id에 맞는 PointHistory return")
+    @Test
+    void 임의의_long_type_값을_user_id에_입력하면_해당_id에_맞는_PointHistory가_return() {
+
+        // given
+        long userId = 100L;
+
+        when(pointHistoryTable.selectAllByUserId(userId))
+                .thenReturn(List.of(
+                        new PointHistory(1L, userId, 2000L, TransactionType.CHARGE,100L ),
+                        new PointHistory(2L, userId, 1000L, TransactionType.USE, 200L),
+                        new PointHistory(3L, userId, 3000L, TransactionType.CHARGE, 300L)
+                ));
+
+        // when // then
+        assertThat(pointService.selectAllById(userId)).hasSize(3)
+                .extracting("id", "userId", "amount", "type", "updateMillis")
+                .containsExactly(
+                        tuple(1L, userId, 2000L, TransactionType.CHARGE,100L ),
+                        tuple(2L, userId, 1000L, TransactionType.USE, 200L),
+                        tuple(3L, userId, 3000L, TransactionType.CHARGE, 300L)
+                );
+
+        Mockito.verify(pointHistoryTable).selectAllByUserId(userId);
+
+
     }
 
 
